9ab0fbb4b341c81c8fa5e460e14f32764f69852a
andres904 <70229352+andres904@users.noreply.github.com>
2020-08-26 19:25:06 -0500

    Add files via upload
    Landing page Versios 2 Easy Pay Food SENA

---
+++ js/main.js
@@ -0,0 +1,42 @@
+AOS.init();
+
+// $(document).ready(function(){
+//     $('.slaider').slick({
+//         dots: true,
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 4,
+//         slidesToScroll: 1,
+//         responsive: [
+//           {
+//             breakpoint: 1025,
+//             settings: {
+//               slidesToShow: 3,
+//               slidesToScroll: 1,
+//               infinite: true,
+//               dots: true
+//             }
+//           },
+//           {
+//             breakpoint: 780,
+//             settings: {
+//               slidesToShow: 2,
+//               slidesToScroll: 1
+//             }
+//           },
+//           {
+//             breakpoint: 480,
+//             settings: {
+//               slidesToShow: 1,
+//               slidesToScroll: 1
+//             }
+//           }
+//           // You can unslick at a given breakpoint now by adding:
+//           // settings: "unslick"
+//           // instead of a settings object
+//         ]
+//       });
+              
+//   });
+
+
